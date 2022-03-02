7735fdf1e95bdb8788af8f5a3d89000df709cb16
fernandakramer <86860877+fernandakramer@users.noreply.github.com>
2022-03-02 14:24:08 -0800

    fourth commit

--- components/ScreenOne.js
+++ components/ScreenOne.js
@@ -2,18 +2,13 @@ import * as React from 'react';
 import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
 import { WebView } from "react-native-webview";
 
-//import GoBackButton from './GoBack';
-
-
 export default function ScreenOne({navigation, route}) {  
     const params = route.params;
     console.log(params.paramName);
     return (
       <View style={styles.screenOne}>
-        <View style={styles.sound}>
             <WebView
                 source={{uri: params.paramName}}/>
-        </View>
       </View>
     );
   }
@@ -24,27 +19,4 @@ const styles = StyleSheet.create({
         alignItems: 'center', 
         justifyContent: 'center',
     },
-    nav_bar: {
-        maxHeight: '15%',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    back: {
-        maxWidth: '15%',
-        alignSelf: 'center',
-    },
-    title_box: {
-        width: '85',
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    title: {
-        color: 'white',
-        fontFamily: "ArialRoundedMTBold",
-        fontSize: 11,
-        textAlign: 'center',
-    },
-    sound: {
-        justifyContent:'center',
-    }
   });
